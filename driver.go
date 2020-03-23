da3d0c719cd1c9c846f929e89fc08b2a90e082d2
Marcelo <baquelo@gmail.com>
2020-03-23 11:31:43 -0300

    Created endpoint to find driver by id Created the Dockerfile

--- driver.go
+++ driver.go
@@ -1,14 +1,24 @@
 package main
 
 import (
+	"encoding/json"
 	"github.com/gorilla/mux"
 	"io/ioutil"
 	"net/http"
 	"os"
 )
 
-func loadDrivers(file string) []byte {
-	jsonFile, err := os.Open(file)
+type Driver struct {
+	Uuid string `json:"uuid"`
+	Name string `json:"name"`
+}
+
+type Drivers struct {
+	Drivers []Driver
+}
+
+func loadDrivers() []byte {
+	jsonFile, err := os.Open("drivers.json")
 	if err != nil {
 		panic(err.Error())
 	}
@@ -24,12 +34,28 @@ func loadDrivers(file string) []byte {
 }
 
 func ListDrivers(w http.ResponseWriter, r *http.Request) {
-	drivers := loadDrivers("drivers.json")
+	drivers := loadDrivers()
 	w.Write([]byte(drivers))
+}
+
+func GetDriverById(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	data := loadDrivers()
+
+	var drivers Drivers
+	json.Unmarshal(data, &drivers)
+
+	for _, v := range drivers.Drivers {
+		if v.Uuid == vars["id"] {
+			driver, _ := json.Marshal(v)
+			w.Write([]byte(driver))
+		}
+	}
 }
 
 func main() {
 	r := mux.NewRouter()
 	r.HandleFunc("/drivers", ListDrivers)
+	r.HandleFunc("/drivers/{id}", GetDriverById)
 	http.ListenAndServe(":8081", r)
 }
